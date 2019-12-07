63ed5fea5646542291e3c21f036d17e72abef890
stjhin <stjhin@gmail.com>
2019-12-07 16:33:05 -0500

    css styling fix img

--- index.js
+++ index.js
@@ -11,18 +11,21 @@ $('#beer-cta').click(function(){
 });
 
 app.displayBeer = function(beer){
+    //console.log(beer.length);
 
+    let totalSearch = beer.length;
     // To recycle results
     $(".beer-card").empty();
+    $("#searchTotal").empty();
 
     beer.forEach(function(beer) {
-        console.log(beer);
-        
-        console.log(beer.name);
-        console.log(beer.image_url);
-        console.log(beer.description);
-        console.log(beer.first_brewed);
-        console.log(beer.food_pairing);
+        //console.log(beer);
+
+        //console.log(beer.name);
+        //console.log(beer.image_url);
+        //console.log(beer.description);
+        //console.log(beer.first_brewed);
+        //console.log(beer.food_pairing);
 
     // Things that need to get pulled from API
         let beerImage = beer.image_url;
@@ -33,34 +36,40 @@ app.displayBeer = function(beer){
 
     // Append HTML for the card All Beer Result
     const htmlAppend =
-    `<section class="section is-small">
-    <div class="card card-rounded">
-    <div class="card-image">
-        <figure class="image is-2by3"><img src="${beerImage}"></figure>
-    <div class="card-content">
-        <div class="media">
-        <div class="media-content">
-            <p class="title beer-text is-2">${beerName}</p>
-            <p class="subtitle beer-text is-4">${beerFood}</p>
-        </div>
-        </div>
-        <div class="content">
-        <p class="title beer-text is-3">${beerDesc}</p>
-        <br>
-        <time datetime="title is-4 beer-brew 2016-1-1">First Brewed: ${beerFirst}</time>
-        </div>
-        </div>
-    </div>
-    </div>
-    </section>`;
+    `<section class="column is-one-third">
+        <div class="card card-rounded">
+                <div class="card-image">
+                    <figure class="image beer-img"><img src="${beerImage}"></figure>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title beer-text is-5"><mark>${beerName}</mark></p>
+                                <p class="subtitle beer-text is-5">${beerFood}</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="title beer-text is-5">${beerDesc}</p>
+                            <br>
+                            <time datetime="title is-4 beer-brew 2016-1-1">First Brewed: ${beerFirst}</time>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>`;
+
     $(".beer-card").append(htmlAppend);
-    })
+    });
+
+    const searchAppend = 
+    `<div class="subtitle beer-card">Total Search Result: ${totalSearch}</div>`;
+
+    $("#searchTotal").append(searchAppend);
 };
 
 app.getBeer = function(food){
 // console.log('get a Beer from API');
     $.ajax({
-    url: `https://api.punkapi.com/v2/beers?page=1&per_page=50`,
+    url: `https://api.punkapi.com/v2/beers?page=1&per_page=80`,
     method: 'GET',
     dataType: 'json',
     data: {
